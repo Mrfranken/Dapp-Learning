4dd9284c7af8b102587736c0f25530c793137d12
VinceWen <973507837@qq.com>
2022-06-06 23:15:14 +0800

    update

---
+++ basic/01-web3js-deploy-test/balances.js
@@ -0,0 +1,33 @@
+const {ethers} = require('ethers');
+require('dotenv').config()
+const privateKey = process.env.PRIVATE_KEY;
+const infura_id = process.env.INFURA_ID;
+console.log('privateKey is: ' + privateKey);
+console.log('infura id is: ' + infura_id);
+
+const provider = new ethers.providers.InfuraProvider('kovan', infura_id)
+
+const addressFrom = '0x998ede1C7B8d2F8099F0fBde395f30e76AeC7BDd';
+const addressTo = '0xA6353f9f077d2CE0DAd84E1BE837f6802e5772DF';
+var g;
+
+const getBalance_USDT = async () => {
+    provider.getBalance(addressFrom).then()
+    g = ethers.utils.formatEther(await provider.getBalance(addressFrom))
+
+};
+a = getBalance_USDT();
+console.log(g)
+
+
+
+// const balances = async () => {
+//     const balanceFrom = ethers.utils.formatEther(await provider.getBalance(addressFrom));
+//
+//     const balanceTo = ethers.utils.formatEther(await provider.getBalance(addressTo));
+//
+//     console.log(`The balance of ${addressFrom} is: ${balanceFrom} ETH`);
+//     console.log(`The balance of ${addressTo} is: ${balanceTo} ETH`);
+// };
+//
+// balances();
---
+++ basic/01-web3js-deploy-test/transaction.js
@@ -0,0 +1,64 @@
+const {ethers} = require('ethers');
+require('dotenv').config()
+const privateKey = process.env.PRIVATE_KEY;
+const infura_id = process.env.INFURA_ID;
+console.log('privateKey is: ' + privateKey);
+console.log('infura id is: ' + infura_id);
+
+const provider = new ethers.providers.InfuraProvider('kovan', infura_id)
+
+
+const account_from = {
+    privateKey: privateKey,
+};
+const addressTo = '0xA6353f9f077d2CE0DAd84E1BE837f6802e5772DF';
+
+let wallet = new ethers.Wallet(account_from.privateKey, provider);
+provider.getBalance('0x998ede1C7B8d2F8099F0fBde395f30e76AeC7BDd').then((b) => {
+    console.log(b)
+})
+
+
+const send = async () => {
+    console.log(`Attempting to send transaction from ${wallet.address} to ${addressTo}`);
+
+    // send eth
+    // const tx = {
+    //     to: addressTo,
+    //     value: ethers.utils.parseEther('0.05'),
+    // };
+
+    // send msg
+    // const tx = {
+    //     to: addressTo,
+    //     // value: ethers.utils.parseEther('0.05'),
+    //     data: "0x496c6f7665457468657265756d"
+    // };
+
+    // send msg or ether and configure gasLimit and gasPrice
+    const tx = {
+        to: addressTo,
+        value: ethers.utils.parseEther('0.01'),
+        data: "0x496c6f7665457468657265756d",
+        gasLimit: 21500,
+        gasPrice: ethers.utils.parseUnits('1.5', 'gwei')
+    };
+
+    // const createReceipt = await wallet.sendTransaction(tx);
+    // await createReceipt.wait();
+    // console.log(`Transaction successful with hash: ${createReceipt.hash}`);
+    wallet.sendTransaction(tx).then((msg) => {
+        console.log(JSON.stringify(msg));
+        console.log(`Transaction successful with hash: ${msg.hash}`);
+    })
+
+    // wallet.signTransaction(tx).then((signedTX) => {
+    //     provider.sendTransaction(signedTX).then(console.log);
+    // })
+
+};
+
+send().catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
